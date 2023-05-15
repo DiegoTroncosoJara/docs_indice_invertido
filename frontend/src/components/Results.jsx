263380c9430ab7adfa8de9e6953ca7a2a51ce1a4
DiegoTroncosoJara <diego.troncosoojara@gmail.com>
2023-05-15 17:15:15 -0400

    fix: variables declaration

--- frontend/src/components/Results.jsx
+++ frontend/src/components/Results.jsx
@@ -1,39 +1,25 @@
-import { Card } from "./Card"
+import { Card } from "./Card";
 
-function ListOfResults ({ json }){
-  return(
-    // <div className="grid grid-cols-4 gap-4 pt-10 justify-items-center">
-    //   {
-    //     json.map(obj => (
-    //     <Card url={obj.url} title={obj.title} key={obj.id}> {obj.content} </Card>
-    //     ))
-    //   }
-    // </div>
+function ListOfResults({ json }) {
+  return (
     <div className="container mx-auto">
       <div className="grid grid-cols-1 gap-4 pt-10 md:grid-cols-2 lg:grid-cols-4">
-         {
-         json.map(obj => (
-         <Card url={obj.url} title={obj.title} key={obj.id}> {obj.content} </Card>
-         ))
-       }
+        {json.map((obj) => (
+          <Card url={obj.url} title={obj.title} key={obj.id}>
+            {obj.content.slice(0, 100)}
+          </Card>
+        ))}
       </div>
-
     </div>
-  )
+  );
 }
 
-function NoResults () {
-  return(
-    <h1>No se encontraron resultados para esta búsqueda.</h1>
-  )
+function NoResults() {
+  return <h1>No se encontraron resultados para esta búsqueda.</h1>;
 }
 
-export function Results( {json} ){
-    const hasResults = json?.length > 0
+export function Results({ json }) {
+  const hasResults = json?.length > 0;
 
-    return (
-      hasResults
-        ? <ListOfResults json={json} />
-        : <NoResults />
-    )
+  return hasResults ? <ListOfResults json={json} /> : <NoResults />;
 }
--- frontend/src/hooks/useResults.js
+++ frontend/src/hooks/useResults.js
@@ -1,14 +1,61 @@
 
+const getSearchResponse= async (query) => {
+  const data = await fetch(
+    `http://0.0.0.0:8000/api/elasticsearch/search?q=${query}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // Manipular los datos de respuesta
+      return data;
+    })
+    .catch((error) => {
+      // Manejar errores
+      console.error(error);
+    });
 
-import json from "../db/obj.json";
-
-export function useResults (){
-    const results = json
-    const mappedResults = results?.map(res => ({
-      id: res.id,
-      title: res.title,
-      content: res.content,
-      url: res.url
-    }))
-    return {results: mappedResults}
+  switch (data?.success) {
+    case true:
+      return data.data;
+    default:
+      return [];
   }
+};
+
+const getAllResults= async () => {
+  const data = await fetch(`http://0.0.0.0:8000/api/elasticsearch/search`)
+    .then((response) => response.json())
+    .then((data) => {
+      // Manipular los datos de respuesta
+      return data;
+    })
+    .catch((error) => {
+      // Manejar errores
+      console.error(error);
+    });
+
+  switch (data?.success) {
+    case true:
+      return data.data;
+    default:
+      return [];
+  }
+};
+
+export async function useResults(query) {
+  let results;
+
+  if (query === undefined || query === null || query === "") {
+    results = await getAllResults();
+  } else {
+    results = await getSearchResponse(query);
+  }
+
+  const mappedResults = results?.map((res) => ({
+    id: res.link,
+    title: res.maintitle,
+    content: res.content,
+    url: res.link,
+  }));
+
+  return mappedResults;
+}
