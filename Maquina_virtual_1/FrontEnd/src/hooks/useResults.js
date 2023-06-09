2546c240b4c467c87de4ce1b38bd96d7acbe8b4f
DiegoTroncosoJara <diego.troncosoojara@gmail.com>
2023-06-08 22:31:13 -0400

    se agrega funcionalidad para varias busquedas

--- Maquina_virtual_1/FrontEnd/src/hooks/useResults.js
+++ Maquina_virtual_1/FrontEnd/src/hooks/useResults.js
@@ -21,6 +21,31 @@ const getSearchResponse= async (query) => {
   }
 };
 
+const getMultiWordSearchResponse= async (query) => {
+
+  let multi_searchs =  query.join("+");
+
+  const data = await fetch(
+    `http://localhost:8000/api/elasticsearch/search?q=${multi_searchs}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // Manipular los datos de respuesta
+      return data;
+    })
+    .catch((error) => {
+      // Manejar errores
+      console.error(error);
+    });
+
+  switch (data?.success) {
+    case true:
+      return data.data;
+    default:
+      return [];
+  }
+};
+
 const getAllResults= async () => {
   const data = await fetch(`http://localhost:8000/api/elasticsearch/search`)
     .then((response) => response.json())
@@ -41,16 +66,27 @@ const getAllResults= async () => {
   }
 };
 
+
+
 // hook que entrega resultados a partir de una busqueda
-export async function useResults(query) {
+export async function useResults(query) {  
   let results;
 
+  // Si no hay palabras en el buscador se muestran todos los resultados
   if (query === undefined || query === null || query === "") {
     results = await getAllResults();
   } else {
-    results = await getSearchResponse(query);
-  }
+    let arr_words = query.split(' ');
 
+    // Se muestran varias busquedas
+    if(arr_words > 1){
+      results = await getMultiWordSearchResponse(arr_words);
+    
+    // Se muestra una busqueda
+    }else{
+      results = await getSearchResponse(arr_words);
+    }
+  }
   const mappedResults = results?.map((res) => ({
     id: res.link,
     title: res.maintitle,
